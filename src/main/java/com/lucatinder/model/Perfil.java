587f961d00914ba79a708a0cf39ef82e3dc6e4b6
Escaleritero <escaleritero1810@hotmail.com>
2019-10-17 16:55:25 +0200

    Create Perfil.java

---
+++ src/main/java/com/lucatinder/model/Perfil.java
@@ -0,0 +1,33 @@
+package com.lucatinder.model;
+
+import java.util.Date;
+
+public class Perfil {
+
+	private int id;
+	private String alias, nombre, descripcion;
+	private boolean genero;
+	private Date fecha_de_nacimiento;
+	
+	public int getId() {
+		return id;
+	}
+	public String getAlias() {
+		return alias;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public boolean isGenero() {
+		return genero;
+	}
+	public Date getFecha_de_nacimiento() {
+		return fecha_de_nacimiento;
+	}
+	
+	
+	
+}
