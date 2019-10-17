b78706bfd985d234fe21d10fe56dd5a7664802be
Escaleritero <escaleritero1810@hotmail.com>
2019-10-17 17:10:33 +0200

    Update Perfil.java

--- src/main/java/com/lucatinder/model/Perfil.java
+++ src/main/java/com/lucatinder/model/Perfil.java
@@ -7,8 +7,25 @@ public class Perfil {
 	private int id;
 	private String alias, nombre, descripcion;
 	private boolean genero;
-	private Date fecha_de_nacimiento;
+	private Date fecha_de_nacimiento;		
 	
+	public Perfil(int id, String alias, String nombre, String descripcion, boolean genero, Date fecha_de_nacimiento) {
+		super();
+		this.id = id;
+		this.alias = alias;
+		this.nombre = nombre;
+		this.descripcion = descripcion;
+		this.genero = genero;
+		this.fecha_de_nacimiento = fecha_de_nacimiento;
+	}
+	
+	public Perfil() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+
+
 	public int getId() {
 		return id;
 	}
