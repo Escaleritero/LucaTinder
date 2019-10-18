e90cbd8faf1931bbd62c28606126f8619735c28c
Escaleritero <escaleritero1810@hotmail.com>
2019-10-18 15:05:59 +0200

    cambios

--- src/main/java/com/lucatinder/servicios/IServicios.java
+++ src/main/java/com/lucatinder/servicios/IServicios.java
@@ -1,7 +1,13 @@
 package com.lucatinder.servicios;
 
+import java.util.List;
+
 import com.lucatinder.model.Perfil;
 
 public interface IServicios {
-	public String addPerfil (Perfil perfil); 
+	
+	public String addPerfil (Perfil perfil); 	
+	public List<Perfil>listaPerfiles(String alias);
+	public Perfil validarPerfil(String alias);
+	
 }
--- src/main/java/com/lucatinder/servicios/ServiciosImpl.java
+++ src/main/java/com/lucatinder/servicios/ServiciosImpl.java
@@ -1,5 +1,7 @@
 package com.lucatinder.servicios;
 
+import java.util.List;
+import java.util.ArrayList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -8,13 +10,38 @@ import com.lucatinder.model.Perfil;
 
 @Service
 public class ServiciosImpl  implements IServicios{
+	
 	@Autowired
 	IPerfilRepositorio iPerfilRepositorio;
 	
+	List aux=iPerfilRepositorio.
+	
 	@Override
-	public String addPerfil(Perfil perfil) {
-		
+	public String addPerfil(Perfil perfil) {		
 		return null;
 	}
+
 	
+	public Perfil validarPerfil (String alias){
+		return null;
+		//Descomentar cuando el método validarPerfil esté creado
+		//return iPerfilRepositorio.validarPerfil( String alias);
+		}
+
+	
+	
+	@Override
+	public List<Perfil> listaPerfiles(String alias) {
+		
+		if() 
+		{
+			//Si hay 20 perfiles
+			return iPerfilRepositorio.listaPerfiles();
+			
+		}
+		else {
+			//
+		}
+		return utilidades.generadorDePerfil();
+	}
 }
