6075acd93b145bdb0a30c252efef683be1a90079
Edmund Home <edmundhome@hotmail.com>
2019-10-20 18:37:02 +0200

    g

---
+++ src/main/java/com/lucatinder/control/ControlMVC.java
@@ -0,0 +1,65 @@
+package com.lucatinder.control;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.lucatinder.model.Perfil;
+import com.lucatinder.servicios.IServicios;
+
+@Controller
+public class ControlMVC {
+
+	@Autowired
+	private IServicios iservicios;
+
+	private static final Logger logger = LoggerFactory.getLogger(ControlMVC.class);
+
+	@GetMapping("/")
+	public String inicio(Model model) {
+		logger.info("--en INICIO");
+		return "index";
+	}
+/*
+	@GetMapping("/registro") // alta
+	public String newPerfil(Model model) {
+		logger.info("--NEW");
+		model.addAttribute("perfil", new Perfil());
+		return "registro";
+	}
+
+	@PostMapping("/registro") // alta
+	public String addPerfil(@ModelAttribute Perfil perfil) {
+		logger.info("--ADD");
+		iservicios.addPerfil(perfil);
+		if (perfil == null) {
+			return "registro";
+		} else {
+			return "principal";
+		}
+	}
+
+
+	@PostMapping("/login")
+	public String loginPerfil(@ModelAttribute String alias, Model model) {
+		Perfil perfil = iservicios.validarPerfil(alias);
+		List<Perfil> listaPerfil = iservicios.listaPerfiles(perfil.getId());
+		logger.info("--ABRIR SESION");
+		if (perfil != null) {
+			model.addAttribute("perfil", perfil);
+			model.addAttribute("lista", listaPerfil);
+			return "principal";
+		} else {
+			return "index";
+		}
+	}
+	*/
+
+}
--- src/main/java/com/lucatinder/model/Perfil.java
+++ src/main/java/com/lucatinder/model/Perfil.java
@@ -5,75 +5,98 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "perfiles")
 public class Perfil {
 
 	@Id
 	@GeneratedValue
 	@Column (name = "id_perfiles")
-	//@ManyToOne/OneToMany/JoinColumn...??
 	private int id;
-	private String alias, nombre, descripcion;
-	private boolean genero;
-	private Date fecha_de_nacimiento;		
 	
-	public Perfil(int id, String alias, String nombre, String descripcion, boolean genero, Date fecha_de_nacimiento) {
-		super();
-		this.id = id;
-		this.alias = alias;
-		this.nombre = nombre;
-		this.descripcion = descripcion;
-		this.genero = genero;
-		this.fecha_de_nacimiento = fecha_de_nacimiento;
-	}
+	@Column (name = "alias")
+	private String alias;
+	
+	@Column (name = "nombre")
+	private String nombre;
+	
+	@Column (name = "genero")
+	private boolean genero;
+	
+	@Column (name = "fecha_de_nacimiento")
+	private Date fecha_de_nacimiento;
+	
+	@Column (name = "descripcion")
+	private String descripcion;
 	
 	public Perfil() {
 		super();
-		// TODO Auto-generated constructor stub
 	}
 
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public void setAlias(String alias) {
+	public Perfil(String alias, String nombre, boolean genero, Date fecha_de_nacimiento) {
+		super();
 		this.alias = alias;
-	}
-
-	public void setNombre(String nombre) {
 		this.nombre = nombre;
-	}
-
-	public void setDescripcion(String descripcion) {
-		this.descripcion = descripcion;
-	}
-
-	public void setGenero(boolean genero) {
 		this.genero = genero;
-	}
-
-	public void setFecha_de_nacimiento(Date fecha_de_nacimiento) {
 		this.fecha_de_nacimiento = fecha_de_nacimiento;
 	}
 
+	public Perfil(String alias, String nombre, boolean genero, Date fecha_de_nacimiento, String descripcion) {
+		super();
+		this.alias = alias;
+		this.nombre = nombre;
+		this.genero = genero;
+		this.fecha_de_nacimiento = fecha_de_nacimiento;
+		this.descripcion = descripcion;
+	}
+	
+	public Perfil(int id, String alias, String nombre, boolean genero, Date fecha_de_nacimiento, String descripcion) {
+		super();
+		this.id = id;
+		this.alias = alias;
+		this.nombre = nombre;
+		this.genero = genero;
+		this.fecha_de_nacimiento = fecha_de_nacimiento;
+		this.descripcion = descripcion;
+	}
+	
 	public int getId() {
 		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
 	}
 	public String getAlias() {
 		return alias;
 	}
+	public void setAlias(String alias) {
+		this.alias = alias;
+	}
 	public String getNombre() {
 		return nombre;
 	}
-	public String getDescripcion() {
-		return descripcion;
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
 	}
 	public boolean isGenero() {
 		return genero;
 	}
+	public void setGenero(boolean genero) {
+		this.genero = genero;
+	}
 	public Date getFecha_de_nacimiento() {
 		return fecha_de_nacimiento;
+	}
+	public void setFecha_de_nacimiento(Date fecha_de_nacimiento) {
+		this.fecha_de_nacimiento = fecha_de_nacimiento;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
 	}
 	
 }
