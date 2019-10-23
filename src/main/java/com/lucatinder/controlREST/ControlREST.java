1838a79eba39f5e520fc2685ea9328a80d6916e4
Escaleritero <escaleritero1810@hotmail.com>
2019-10-23 15:48:56 +0200

    CrossingOrigin

--- src/main/java/com/lucatinder/controlREST/ControlREST.java
+++ src/main/java/com/lucatinder/controlREST/ControlREST.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -27,6 +28,7 @@ import ch.qos.logback.classic.Logger;
 
 @RestController
 @RequestMapping("/lucatinder")
+@CrossOrigin(origins = "*")
 public class ControlREST {
 
 	@Autowired
